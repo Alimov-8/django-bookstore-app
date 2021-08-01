82f52a8ec91ba823dc118f3a5c1177da97f0fa24
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-01 15:24:50 +0500

    Auth URLs and Views

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), 
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')), # new
+    
+    # Local apps
+    path('', include('pages.urls')),
 ]
