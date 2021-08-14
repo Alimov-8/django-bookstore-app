385e12eddcadb1f8397ba083fa3b15c1d3aae0e2
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-14 16:03:42 +0500

    DetailView with get_absoulte_url | pk,id,slug,uuid

--- books/models.py
+++ books/models.py
@@ -1,15 +1,27 @@
+import uuid
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
 
 class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[str(self.id)])
 
     class Meta:
         verbose_name = 'Book 📘'
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 # Create your views here.
@@ -9,3 +9,9 @@ class BookListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_detail.html'
